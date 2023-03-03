6cc3bfa2034dd578f29d569d53fd04f775759bc8
MeltemElgun <113603458+MeltemElgun@users.noreply.github.com>
2023-03-04 00:35:58 +0300

    s10g2-redux-filmler-completed

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
 
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
 
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import reducer from './reducers';
+import { legacy_createStore as createStore } from "redux";
+import { Provider } from "react-redux";
+import rootReducer from "./reducers/";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const myStore = createStore(rootReducer);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <App />
+    <Provider store={myStore}>
+      <App />
+    </Provider>
   </BrowserRouter>
 );
 
